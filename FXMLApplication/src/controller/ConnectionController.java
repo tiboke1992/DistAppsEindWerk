f23820c777dc85dc5b2f4a8e4b83a020e0968f38
Tibo <tibo@192.168.0.107>
2012-12-14 20:58:53 +0100

    Klanten toevoegen

---
+++ FXMLApplication/src/controller/ConnectionController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import enitityControllers.AdresFacadeRemote;
+import enitityControllers.BestellingFacadeRemote;
+import enitityControllers.KlantFacadeRemote;
+import enitityControllers.MagazijnFacadeRemote;
+import enitityControllers.ProductFacadeRemote;
+import enitityControllers.VasteKlantFacadeRemote;
+import fxmlapplication.Main;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author tibo
+ */
+public class ConnectionController {
+
+    //**Context en facades Declaratie
+    Context c;
+    AdresFacadeRemote adresDB;
+    BestellingFacadeRemote bestellingDB;
+    KlantFacadeRemote klantDB;
+    MagazijnFacadeRemote magazijnDB;
+    ProductFacadeRemote productDB;
+    VasteKlantFacadeRemote vasteKlantDB;
+    //**Context en facades Declaratie;;;;;
+
+    public ConnectionController() {
+        this.Initialize();
+    }
+
+    private void Initialize() {
+        try {
+            this.c = new InitialContext();
+            this.adresDB = (AdresFacadeRemote) c.lookup("java:global/EJBAdministratie/AdresFacade");
+            this.bestellingDB = (BestellingFacadeRemote) c.lookup("java:global/EJBAdministratie/BestellingFacade");
+            this.klantDB = (KlantFacadeRemote) c.lookup("java:global/EJBAdministratie/KlantFacade");
+            this.magazijnDB = (MagazijnFacadeRemote) c.lookup("java:global/EJBAdministratie/MagazijnFacade");
+            this.productDB = (ProductFacadeRemote) c.lookup("java:global/EJBAdministratie/ProductFacade");
+            this.vasteKlantDB = (VasteKlantFacadeRemote) c.lookup("java:global/EJBAdministratie/VasteKlantFacade");
+        } catch (NamingException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public AdresFacadeRemote getAdresDB() {
+        return adresDB;
+    }
+
+    public BestellingFacadeRemote getBestellingDB() {
+        return bestellingDB;
+    }
+
+    public KlantFacadeRemote getKlantDB() {
+        return klantDB;
+    }
+
+    public MagazijnFacadeRemote getMagazijnDB() {
+        return magazijnDB;
+    }
+
+    public ProductFacadeRemote getProductDB() {
+        return productDB;
+    }
+
+    public VasteKlantFacadeRemote getVasteKlantDB() {
+        return vasteKlantDB;
+    }
+    
+    
+}
--- FXMLApplication/src/ui/klant/FXMLNieuweKlantController.java
+++ FXMLApplication/src/ui/klant/FXMLNieuweKlantController.java
@@ -4,10 +4,19 @@
  */
 package ui.klant;
 
+import controller.ConnectionController;
+import enitityControllers.KlantFacadeRemote;
+import entitys.Klant;
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -20,13 +29,52 @@ public class FXMLNieuweKlantController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    ConnectionController connController;
+    KlantFacadeRemote klantDB;
     @FXML 
     AnchorPane p;
+    @FXML
+    private TextField naam;
+    @FXML
+    private TextField voorNaam;
+    @FXML
+    private TextField geboorteDatum;
+    @FXML
+    private TextField telefoonNR;
+    @FXML
+    private Button save;
+    @FXML
+    private Label boodschap;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        connController = new ConnectionController();
+        klantDB = connController.getKlantDB();
+        save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                //Klant toevoegen
+                addKlant();
+            }
+        });
     }    
     public AnchorPane getAnchorPane(){
         return this.p;
+    }
+    public void addKlant(){
+        String kVoornaam = this.voorNaam.getText();
+        String kNaam = this.naam.getText();
+        String kGeboorteDatum = this.geboorteDatum.getText();
+        Date date = Date.valueOf(kGeboorteDatum);
+        String kTelefoorNR = this.telefoonNR.getText();
+        this.klantDB.create(new Klant(kVoornaam, kNaam, date, kTelefoorNR));
+        this.emptyFields();
+    }
+    
+    public void emptyFields(){
+        this.naam.clear();
+        this.voorNaam.clear();
+        this.geboorteDatum.clear();
+        this.telefoonNR.clear();
+        this.boodschap.setText("Klant succesvol toegevoegd");
     }
 }
