4147e9a8681eee0bf89e52ad1a19ab31e40f212a
Tibo <tibo@192.168.0.107>
2012-12-15 16:53:03 +0100

    Klant overzicht toegevoegd

--- FXMLApplication/src/controller/DataController.java
+++ FXMLApplication/src/controller/DataController.java
@@ -7,25 +7,32 @@ package controller;
 import enitityControllers.KlantFacadeRemote;
 import entitys.Klant;
 import java.sql.Date;
+import java.util.List;
 
 /**
  *
  * @author tibo
  */
 public class DataController {
+
     private ConnectionController connController;
     private KlantFacadeRemote klantDB;
-    
-    public DataController(){
+
+    public DataController() {
         this.initialize();
     }
-    
-    public void initialize(){
+
+    public void initialize() {
         connController = new ConnectionController();
         klantDB = connController.getKlantDB();
     }
-    
-    public void addNewKlant(String voornaam, String naam, Date geboorteDatum, String telefoonNR){
-        klantDB.create(new Klant(voornaam,naam,geboorteDatum,telefoonNR));
+
+    public void addNewKlant(String voornaam, String naam, Date geboorteDatum, String telefoonNR) {
+        klantDB.create(new Klant(voornaam, naam, geboorteDatum, telefoonNR));
+    }
+
+    public List<Klant> getKlantenLijst() {
+        List<Klant> k = klantDB.findAll();
+        return k;
     }
 }
--- FXMLApplication/src/ui/klant/FXMLOverzichtKlantenController.java
+++ FXMLApplication/src/ui/klant/FXMLOverzichtKlantenController.java
@@ -4,9 +4,22 @@
  */
 package ui.klant;
 
+import controller.DataController;
+import entitys.Klant;
 import java.net.URL;
+import java.sql.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -15,11 +28,38 @@ import javafx.fxml.Initializable;
  */
 public class FXMLOverzichtKlantenController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
+    @FXML
+    private TableView tabel;
+    private List<Klant> klantenLijst;
+    private DataController controller;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        controller = new DataController();
+        klantenLijst = controller.getKlantenLijst();
+        //table maken en invullen
+
+        ObservableList<Klant> list = FXCollections.observableArrayList(klantenLijst);
+        //Kollommen
+        TableColumn voornaamCol = new TableColumn();
+        TableColumn naamCol = new TableColumn();
+        TableColumn gebDatumCol = new TableColumn();
+        TableColumn telefoonCol = new TableColumn();
+        
+        voornaamCol.setText("Voornaam");
+        voornaamCol.setCellValueFactory(new PropertyValueFactory<Klant,String>("voorNaam"));
+        naamCol.setText("Naam");
+        naamCol.setCellValueFactory(new PropertyValueFactory<Klant,String>("naam"));
+        gebDatumCol.setText("Geboorte Datum");
+        gebDatumCol.setCellValueFactory(new PropertyValueFactory<Klant,Date>("geboorteDatum"));
+        telefoonCol.setText("Telefoon nummer");
+        telefoonCol.setCellValueFactory(new PropertyValueFactory<Klant,String>("telefoonNummer"));
+        
+        tabel.setItems(list);
+        tabel.getColumns().clear();
+        tabel.getColumns().addAll(voornaamCol,naamCol,gebDatumCol,telefoonCol);
+        tabel.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
 }
